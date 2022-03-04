6862d5e452538d4bf637b7ce087e61c61f2faf92
video2ster <80967535+video2ster@users.noreply.github.com>
2022-03-04 11:47:23 -0600

    add redirect

--- login.js
+++ login.js
@@ -1,7 +1,8 @@
 var MXLOGIN = new XMLHttpRequest();
-function mx_sendLogin(mxid, password, callback) {
+function mx_sendLogin(mxid, password) {
 	MXLOGIN.onreadystatechange = function () {
-  		callback(JSON.parse(MXLOGIN.responseText));
+		var response = JSON.parse(MXLOGIN.responseText);
+  		window.location.href = "channels?atoken="+response.access_token+"&uid="+response.user_id;
 	}
 	
 	MXLOGIN.open("POST","https://matrix.org/_matrix/client/r0/login");
