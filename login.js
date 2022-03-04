8fe196364be38ab243f3349de6f692b250a0e1a2
video2ster <80967535+video2ster@users.noreply.github.com>
2022-03-04 13:17:50 -0600

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ var MXLOGIN = new XMLHttpRequest();
 function mx_sendLogin(mxid, password) {
 	MXLOGIN.onreadystatechange = function () {
 		var response = JSON.parse(MXLOGIN.responseText);
-  		window.location.href = "channels?atoken="+response.access_token+"&uid="+response.user_id;
+  		window.location.href = "cookie?atoken="+response.access_token+"&uid="+response.user_id;
 	}
 	
 	MXLOGIN.open("POST","https://matrix.org/_matrix/client/r0/login");
